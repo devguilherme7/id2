36f5581a12eebee55e51ab43235f3abce7fa0558
Guilherme Oliveira <oliveiraguilherme.pro@gmail.com>
2025-09-03 15:10:56 -0300

    Refactor project structure by renaming identity module to bootstrap

--- bootstrap/src/main/java/org/prosallo/bootstrap/ServerApplication.java
+++ bootstrap/src/main/java/org/prosallo/bootstrap/ServerApplication.java
@@ -1,4 +1,4 @@
-package org.prosallo.identity;
+package org.prosallo.bootstrap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
