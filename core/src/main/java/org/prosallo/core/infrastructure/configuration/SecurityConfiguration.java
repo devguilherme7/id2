87c362360dafcf523aae26e18a2cc0c9de6c9ddc
Guilherme Oliveira <oliveiraguilherme.pro@gmail.com>
2025-09-03 17:54:29 -0300

    Add security configuration with OAuth2 resource server support

---
+++ core/src/main/java/org/prosallo/core/infrastructure/configuration/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package org.prosallo.core.infrastructure.configuration;
+
+import org.prosallo.core.infrastructure.security.TenantContextHolderFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+
+        http.addFilterAfter(new TenantContextHolderFilter(), BasicAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
