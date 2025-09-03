593381ecc32cfe0ce362acac9caaa1c688cc3e9d
Guilherme Oliveira <oliveiraguilherme.pro@gmail.com>
2025-09-03 20:40:15 -0300

    Add MapStruct dependencies and global mapper configuration

---
+++ core/src/main/java/org/prosallo/core/infrastructure/configuration/GlobalMapperConfiguration.java
@@ -0,0 +1,7 @@
+package org.prosallo.core.infrastructure.configuration;
+
+import org.mapstruct.MapperConfig;
+import org.mapstruct.MappingConstants;
+
+@MapperConfig(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface GlobalMapperConfiguration {}
