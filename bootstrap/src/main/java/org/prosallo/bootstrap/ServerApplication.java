c6c23976841dc931d3d3a870f63559de82b7f68b
Guilherme Oliveira <oliveiraguilherme.pro@gmail.com>
2025-09-03 20:42:18 -0300

    Add JPA repository and entity scanning configuration

--- bootstrap/src/main/java/org/prosallo/bootstrap/ServerApplication.java
+++ bootstrap/src/main/java/org/prosallo/bootstrap/ServerApplication.java
@@ -2,8 +2,12 @@ package org.prosallo.bootstrap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"org.prosallo.*"})
+@EnableJpaRepositories(basePackages = {"org.prosallo.*"})
+@EntityScan(basePackages = {"org.prosallo.*"})
 public class ServerApplication {
 
     public static void main(String[] args) {
