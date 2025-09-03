6329aeed4baa958a15488ea66d4120a29f9292c2
Guilherme Oliveira <oliveiraguilherme.pro@gmail.com>
2025-09-03 20:42:10 -0300

    Add public access to Swagger UI and API docs

--- core/src/main/java/org/prosallo/core/infrastructure/configuration/SecurityConfiguration.java
+++ core/src/main/java/org/prosallo/core/infrastructure/configuration/SecurityConfiguration.java
@@ -15,6 +15,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
 
