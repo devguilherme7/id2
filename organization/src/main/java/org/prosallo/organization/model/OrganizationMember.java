13e15cd4269c3bafc687128d5e0a4b57b1a82048
Guilherme Oliveira <oliveiraguilherme.pro@gmail.com>
2025-09-05 18:06:32 -0300

    Assign SUPER_ADMIN role to organization creator

--- organization/src/main/java/org/prosallo/organization/model/OrganizationMember.java
+++ organization/src/main/java/org/prosallo/organization/model/OrganizationMember.java
@@ -2,10 +2,15 @@ package org.prosallo.organization.model;
 
 import org.prosallo.core.domain.AggregateRoot;
 
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public final class OrganizationMember extends AggregateRoot {
 
     private final Organization organization;
     private final String userId;
+    private final Set<Role> roles = new LinkedHashSet<>();
 
     public OrganizationMember(Organization organization, String userId) {
         this.organization = organization;
@@ -16,11 +21,19 @@ public final class OrganizationMember extends AggregateRoot {
         return new OrganizationMember(organization, userId);
     }
 
+    public void assignRole(Role role) {
+        this.roles.add(role);
+    }
+
     public Organization getOrganization() {
         return organization;
     }
 
     public String getUserId() {
         return userId;
+    }
+
+    public Set<Role> getRoles() {
+        return Collections.unmodifiableSet(this.roles);
     }
 }
--- organization/src/main/java/org/prosallo/organization/service/DefaultOrganizationService.java
+++ organization/src/main/java/org/prosallo/organization/service/DefaultOrganizationService.java
@@ -3,21 +3,33 @@ package org.prosallo.organization.service;
 import org.prosallo.organization.data.OrganizationResponse;
 import org.prosallo.organization.model.Organization;
 import org.prosallo.organization.model.OrganizationMember;
+import org.prosallo.organization.model.OrganizationMemberRoleAssignment;
+import org.prosallo.organization.model.Role;
 import org.prosallo.organization.repository.OrganizationMemberRepository;
+import org.prosallo.organization.repository.OrganizationMemberRoleAssignmentRepository;
 import org.prosallo.organization.repository.OrganizationRepository;
+import org.prosallo.organization.repository.RoleRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class DefaultOrganizationService implements OrganizationService {
 
+    private static final String SUPER_ADMIN_ROLE_NAME = "SUPER_ADMIN";
+
     private final OrganizationRepository organizationRepository;
+    private final RoleRepository roleRepository;
     private final OrganizationMemberRepository organizationMemberRepository;
+    private final OrganizationMemberRoleAssignmentRepository organizationMemberRoleAssignmentRepository;
 
     public DefaultOrganizationService(OrganizationRepository organizationRepository,
-            OrganizationMemberRepository organizationMemberRepository) {
+            RoleRepository roleRepository,
+            OrganizationMemberRepository organizationMemberRepository,
+            OrganizationMemberRoleAssignmentRepository organizationMemberRoleAssignmentRepository) {
         this.organizationRepository = organizationRepository;
+        this.roleRepository = roleRepository;
         this.organizationMemberRepository = organizationMemberRepository;
+        this.organizationMemberRoleAssignmentRepository = organizationMemberRoleAssignmentRepository;
     }
 
     @Override
@@ -26,8 +38,15 @@ public class DefaultOrganizationService implements OrganizationService {
         var organization = Organization.create(organizationName);
         organization = organizationRepository.save(organization);
 
+        var ownerRole = Role.create(organization, SUPER_ADMIN_ROLE_NAME);
+        ownerRole = roleRepository.save(ownerRole);
+
         var owner = OrganizationMember.create(organization, ownerId);
-        organizationMemberRepository.save(owner);
+        owner.assignRole(ownerRole);
+        owner = organizationMemberRepository.save(owner);
+
+        var assignment = OrganizationMemberRoleAssignment.create(owner, ownerRole);
+        organizationMemberRoleAssignmentRepository.save(assignment);
 
         return new OrganizationResponse(organization.getId(), organization.getName());
     }
